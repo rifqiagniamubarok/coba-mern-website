3f47f028ca71e42e1d8e88c84d979e6bf4cf2d4f
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2022-08-13 01:39:13 +0700

    format global state

--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -1,35 +1,5 @@
-const { createStore, combineReducers } = require("redux");
-
-const initialStateHome = {
-  dataBlogs: [],
-};
-
-const HomeReducer = (state = initialStateHome, action) => {
-  if (action.type === "UPDATE_DATA_BLOG") {
-    return {
-      ...state,
-      dataBlogs: action.payload,
-    };
-  }
-
-  return state;
-};
-
-const initialState = {
-  name: "rifqi",
-};
-
-const GlobalReducer = (state = initialState, action) => {
-  if (action.type === "UPDATE_NAME") {
-    return {
-      ...state,
-      name: "agnia",
-    };
-  }
-  return state;
-};
-
-const reducer = combineReducers({ HomeReducer, GlobalReducer });
+import { createStore } from "redux";
+import reducer from "./reducer/reducer";
 
 const store = createStore(reducer);
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useNavigate } from "react-router-dom";
@@ -6,25 +6,16 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  const [dataBlog, setDataBlog] = useState([]);
-  // const { dataBlogs, name } = useSelector((state) => state);
-  const { dataBlogs } = useSelector((state) => state.HomeReducer);
+  const { dataBlog } = useSelector((state) => state.homeReducer);
 
   const dispatch = useDispatch();
 
-  console.log("state data blog global :", dataBlogs);
   useEffect(() => {
-    setTimeout(() => {
-      dispatch({ type: "UPDATE_NAME" });
-    }, 3000);
-
     axios
       .get("http://localhost:4000/v1/blog/posts?page=1&perPage=2")
       .then((result) => {
-        console.log("data API :", result.data);
         const responseAPI = result.data;
 
-        // setDataBlog(responseAPI.data);
         dispatch({ type: "UPDATE_DATA_BLOG", payload: responseAPI.data });
       })
       .catch((err) => {
@@ -38,10 +29,9 @@ const Home = () => {
       <div className="create-wrapper">
         <Button title="create blog" onClick={() => navigate("/create-blog")} />
       </div>
-      {/* <p>{name}</p> */}
       <Gap height={20} />
       <div className="content-wrapper">
-        {dataBlogs.map((blog) => {
+        {dataBlog.map((blog) => {
           return (
             <BlogItem
               key={blog._id}
